00404011029f1289f963ed3dc788187706a836c0
Brian Fogarty <bri@nfogarty.me>
2020-08-08 23:42:54 -0400

    [pkgs/mole] create mole package

---
+++ pkgs/mole/default.nix
@@ -0,0 +1,31 @@
+{ stdenv, fetchurl, autoPatchelfHook }:
+
+stdenv.mkDerivation rec {
+  name = "mole-${version}";
+  version = "0.5.0";
+
+  src = fetchurl {
+    url = "https://github.com/davrodpin/mole/releases/download/v${version}/mole${version}.darwin-amd64.tar.gz";
+    sha256 = "618e29266e4fd42bfbbe9c3b9c030a656f1a24344d7148928f3f71b8bdef9ab3";
+  };
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+
+  unpackPhase = ''
+    tar -xzf $src
+  '';
+
+  installPhase = ''
+    install -d $out/bin
+    install -m755 mole $out/bin/mole
+  '';
+
+  meta = with stdenv.lib; {
+    homepage = https://davrodpin.github.io/mole/;
+    description = "Easily create SSH tunnels.";
+    license = licenses.mit;
+    platforms = platforms.darwin;
+  };
+}
