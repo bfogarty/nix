207b91b3f3c9c578af1cef35d2cfe7639cd4296d
Brian Fogarty <bri@nfogarty.me>
2021-01-10 17:00:00 -0500

    [home-manager/kitty] grid layout -> splits,stack

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -4,7 +4,7 @@
 
     settings = {
       font_size = "13.0";
-      enabled_layouts = "grid";
+      enabled_layouts = "splits,stack";
       shell = "/Users/brian/.nix-profile/bin/fish";
       macos_option_as_alt = "yes";
       confirm_os_window_close = 1;
@@ -12,6 +12,8 @@
     };
 
     keybindings = {
+      "ctrl+alt+s" = "launch --location=hsplit";
+      "ctrl+alt+v" = "launch --location=vsplit";
       "ctrl+alt+enter" = "launch --cwd=current";
     };
   };
