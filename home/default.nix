5aa304d296dd914863de10d15f83c315c21e762d
Brian Fogarty <brian@fogarty.email>
2022-01-07 16:23:00 -0600

    [home-manager/packages] use python3.9

--- home/default.nix
+++ home/default.nix
@@ -34,10 +34,10 @@ in {
     # TODO helm-diff (installed via helm plugin install)
     mole
     pgcli
-    python3
-    python38Packages.grip
-    python38Packages.ipython
     postgresql_12 # for psql
+    python39
+    python39Packages.grip
+    python39Packages.ipython
     rectangle
     ripgrep
     sentry-cli
