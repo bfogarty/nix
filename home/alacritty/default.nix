d7c2dfe5884d686a07279ce218b869f35b54a335
Brian Fogarty <brian@fogarty.email>
2023-01-10 23:18:06 -0800

    [home-manager/alacritty] font size, opt as meta
    * Increases the default terminal font size.
    * Adds option-as-meta keybindings.

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -6,8 +6,24 @@
 
     settings = {
       font = {
-        size = 13.0;
+        size = 15.0;
       };
+
+      key_bindings = [
+        # unlike kitty, alacritty has no darwin "option as meta" setting
+        # https://github.com/alacritty/alacritty/issues/62
+        {
+          key = "E";
+          mods = "Alt";
+          chars = "\\x1be";
+        }
+        # for zellij
+        { key = "H"; mods = "Alt"; chars = "\\x1bh"; }
+        { key = "J"; mods = "Alt"; chars = "\\x1bj"; }
+        { key = "K"; mods = "Alt"; chars = "\\x1bk"; }
+        { key = "L"; mods = "Alt"; chars = "\\x1bl"; }
+        { key = "N"; mods = "Alt"; chars = "\\x1bn"; }
+      ];
 
       shell = {
         program = "${pkgs.fish}/bin/fish";
