f778734a2d23038acaaafcb46e49adf40c46510b
Brian Fogarty <brian@fogarty.email>
2023-02-19 23:06:46 -0600

    Cachix (#2)

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -9,6 +9,14 @@
   time.timeZone = "America/Chicago";
 
   nix.settings = {
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://bfogarty.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "bfogarty.cachix.org-1:Q44G+kZZesKfq1fMRcFezSt846t5zgQpiDFHTcvrI80="
+    ];
     extra-experimental-features = "nix-command flakes";
   };
 
